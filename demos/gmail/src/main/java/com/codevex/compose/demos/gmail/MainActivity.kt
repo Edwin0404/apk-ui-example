2cac33d0959ff13ffd24e26fbc74734d12fcad12
VASQUEZ VARGAS EDWIN <edwin.vasquez0417@gmail.com>
2024-12-02 01:26:18 -0500

    gmail activity

--- demos/gmail/src/main/java/com/codevex/compose/demos/gmail/MainActivity.kt
+++ demos/gmail/src/main/java/com/codevex/compose/demos/gmail/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.codevex.compose.demos.gmail.ui.theme.ApkUISampleTheme
+import com.codevex.compose.demos.gmail.ui.theme.GmailTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ApkUISampleTheme {
+            GmailTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ApkUISampleTheme {
+    GmailTheme {
         Greeting("Android")
     }
 }
